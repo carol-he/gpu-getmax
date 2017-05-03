14f784b1ea1a57e222ec7e564baa7da95873c39d
Carol He <ch2727@nyu.edu>
2017-05-02 21:24:19 -0400

    code compiles! seg fault!

--- maxgpu.cu
+++ maxgpu.cu
@@ -1,8 +1,13 @@
+#include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+#define THREADS_PER_BLOCK 512
+
+//function declarations
 unsigned int getmax(unsigned int *, unsigned int);
+__global__ void get_max(unsigned int *num);
 
 int main(int argc, char *argv[])
 {
@@ -16,8 +21,11 @@ int main(int argc, char *argv[])
        printf("num = size of the array\n");
        exit(1);
     }
-
+    //size is number of threads total
     size = atol(argv[1]);
+
+    //calculates number of blocks
+    unsigned int NUM_BLOCKS = size/THREADS_PER_BLOCK;
 
     numbers = (unsigned int *)malloc(size * sizeof(unsigned int));
     if( !numbers )
@@ -32,24 +40,44 @@ int main(int argc, char *argv[])
       numbers[i] = rand()  % size;
     }
     //create device pointers
-    (unsigned int *) d_numbers;
+    unsigned int *d_numbers;
     //transfer array to device memory
     cudaMalloc((void**) &d_numbers, size * sizeof(unsigned int));
-    cudaMemcpy(d_numbers, numbers, size * sizeof(unsigned int)), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_numbers, numbers, size * sizeof(unsigned int), cudaMemcpyHostToDevice);
+    //sequential
     printf(" The maximum number in the array is: %u\n", getmax(numbers, size));
-    get_max<<<1, 512>>>(d_numbers, size * sizeof(unsigned int));
+    //parallel
+    get_max<<<NUM_BLOCKS, THREADS_PER_BLOCK>>>(d_numbers);
     cudaMemcpy(numbers, d_numbers, size * sizeof(unsigned int), cudaMemcpyDeviceToHost);
     for( i = 0; i < size; i++){
-      printf("element in %d: %u", );
+      printf("element in %d: %u\n", i, d_numbers[i]);
     }
+     printf("The max integer in the array is: %d\n", d_numbers[0]);
     //free device matrices
     cudaFree(d_numbers);
     free(numbers);
     exit(0);
 }
 
-__global__ void get_max(unsigned int* d_numbers, int size){
-  for
+__global__ void get_max(unsigned int* num){
+  unsigned int temp;
+  unsigned int index = threadIdx.x + (blockDim.x * blockIdx.x);
+  unsigned int nTotalThreads = 10;
+
+  while(nTotalThreads > 1){
+    unsigned int halfPoint = nTotalThreads / 2;	// divide by two
+    // only the first half of the threads will be active.
+    if (index < halfPoint){
+      temp = num[ index + halfPoint ];
+      if (temp > num[ index ]) {
+        num[index] = temp;
+      }
+    }
+    __syncthreads();
+
+
+    nTotalThreads = nTotalThreads / 2;	// divide by two.
+  }
 }
 
 /*
